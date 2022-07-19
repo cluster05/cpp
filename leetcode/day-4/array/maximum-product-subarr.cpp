386fd0d302f24a6460d976774b1fc4602133bf80
cluster05 <ajayakumbhar1234@gmail.com>
2022-07-19 19:09:53 +0530

    optimized feature

--- leetcode/day-4/array/maximum-product-subarr.cpp
+++ leetcode/day-4/array/maximum-product-subarr.cpp
@@ -64,3 +64,33 @@ int main() {
 
 
  */
+
+/*
+  optimized
+
+  #include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarrayProduct(vector<int> arr) {
+  int max_ending_here = arr[0];
+  int min_ending_here = arr[0];
+  int max_so_far = arr[0];
+  for (int i = 1; i < arr.size(); i++) {
+    int temp =
+        max({arr[i], arr[i] * max_ending_here, arr[i] * min_ending_here});
+    min_ending_here =
+        min({arr[i], arr[i] * max_ending_here, arr[i] * min_ending_here});
+    max_ending_here = temp;
+    max_so_far = max(max_so_far, max_ending_here);
+  }
+  return max_so_far;
+}
+
+int main() {
+  vector<int> arr = {-2, 3, 0, 0, -10, -100};
+
+  cout << maxSubarrayProduct(arr);
+}
+
+
+ */
